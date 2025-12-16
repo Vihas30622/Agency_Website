bad0fbe151285194c23c354d64c8d7fcf106fd64
Vihas Vattiikuti <2420030622@klh.edu.in>
2025-12-16 13:50:43 +0530

    IRIDESCENSE

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import Iridescence from './Iridescence';
+<Iridescence
+  color={[1, 1, 1]}
+  mouseReact={false}
+  amplitude={0.1}
+  speed={1.0}
+/>
 
 createRoot(document.getElementById("root")!).render(<App />);
